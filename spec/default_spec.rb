71842ed9ed148b2c165298e1963dd52c37e165a8
Jonathan Kinred <jonathan.kinred@team.telstra.com>
2016-07-18 17:58:46 +1000

    Add serverspec tests

---
+++ spec/default_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+set :backend, :exec
+
+context 'install' do
+  describe group('confluence') do
+    it { should exist }
+  end
+
+  describe user('confluence') do
+    it { should exist }
+    it { should belong_to_group 'confluence' }
+    it { should have_login_shell '/sbin/nologin' }
+  end
+end
+
+context 'directories' do
+  [
+    '/opt/atlassian/confluence/5.9.12',
+    '/var/atlassian/application-data/confluence'
+  ].each do |dir|
+    describe file(dir) do
+      it { should be_directory }
+      it { should be_owned_by 'confluence' }
+      it { should be_grouped_into 'confluence' }
+    end
+  end
+end
+
+context 'config files' do
+  [
+    '/opt/atlassian/confluence/5.9.12/conf/server.xml'
+  ].each do |file|
+    describe file(file) do
+      it { should be_file }
+      # its(:content) { should match /zone.*0.0.10.in-addr.arpa/ }
+    end
+  end
+end
+
+context 'server started' do
+  describe file('/opt/atlassian/confluence/5.9.12/logs/catalina.out') do
+    its(:content) { should match(/Server startup in/) }
+  end
+end
+
+context 'service' do
+  describe service('named') do
+    it { should be_running }
+  end
+
+  [8090].each do |port|
+    describe port(port) do
+      it { should be_listening.with('tcp') }
+    end
+  end
+end
